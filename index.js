c8b3136f104903c0db9c13c2303902c78dfc664a
Nilay <nmodi9@gmail.com>
2018-03-08 20:35:56 -0600

    feature: added actual server.js content and options

--- index.js
+++ index.js
@@ -2,22 +2,33 @@
 
 class WildcardServerWebpackPlugin {
 	constructor(options) {
-		this.options = options;
+		const { fileName, portNumber, distFolder } = options;
+		this.fileName = fileName || 'server.js';
+        this.portNumber = portNumber || '3001';
+        this.distFolder = distFolder || 'dist'; 
 	}
 
 	apply(compiler) {
-		const _this = this;
-
-		const content = 'hello world';
+		const content = `
+            var express = require('express');
+            
+            var server = express();
+            server.use('/${this.distFolder}', express.static(__dirname + '/${this.distFolder}'));
+            
+            server.get('/*', function(req, res) {
+                res.sendFile(__dirname + '/index.html');
+            });
+            
+            var port = ${this.portNumber};
+            server.listen(port, function() {
+                console.log('Server listening on port ' + ${this.portNumber});
+            });
+        `;
 
 		compiler.plugin('emit', function(compilation, callback) {
-			compilation.assets['server.js'] = {
-				source: function() {
-					return content;
-				},
-				size: function() {
-					return content.length;
-				}
+			compilation.assets[this.options.fileName] = {
+				source: () => content,
+				size: () => content.length
 			};
 
 			callback();
