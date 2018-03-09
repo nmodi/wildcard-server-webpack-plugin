60e5abc08c790748365c043f31cc251fdde4dcc1
Nilay <nmodi9@gmail.com>
2018-03-08 20:50:21 -0600

    fix: fixed formatting of file and options

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 'use strict';
 
+const DEFAULT_OPTIONS = {
+    serverFileName: 'server.js',
+    portNumber: 3001,
+    htmlFileName: 'index.html'
+  };
+  
+
 class WildcardServerWebpackPlugin {
 	constructor(options) {
-		const { fileName, portNumber, distFolder } = options;
-		this.fileName = fileName || 'server.js';
-        this.portNumber = portNumber || '3001';
-        this.distFolder = distFolder || 'dist'; 
+        this.options = {
+            ...DEFAULT_OPTIONS,
+            ...options,
+          };
 	}
 
 	apply(compiler) {
+        const {
+            serverFileName,
+            htmlFileName, 
+            portNumber
+        } = this.options; 
+
+        const {outputPath} = compiler; 
+
 		const content = `
-            var express = require('express');
-            
-            var server = express();
-            server.use('/${this.distFolder}', express.static(__dirname + '/${this.distFolder}'));
-            
-            server.get('/*', function(req, res) {
-                res.sendFile(__dirname + '/index.html');
-            });
-            
-            var port = ${this.portNumber};
-            server.listen(port, function() {
-                console.log('Server listening on port ' + ${this.portNumber});
-            });
+var express = require('express');
+
+var server = express();
+server.use('/${outputPath}', express.static(__dirname + '/${outputPath}'));
+
+server.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/${htmlFileName}');
+});
+
+var port = ${portNumber};
+server.listen(port, function() {
+    console.log('Server listening on port ' + ${portNumber});
+});
         `;
 
 		compiler.plugin('emit', function(compilation, callback) {
-			compilation.assets[this.options.fileName] = {
+			compilation.assets[serverFileName] = {
 				source: () => content,
 				size: () => content.length
 			};
