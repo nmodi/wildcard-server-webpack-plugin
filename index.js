465ba5cde15e50fc40d7aff20418d2ee1dc44b28
Nilay <nmodi9@gmail.com>
2018-03-08 21:03:44 -0600

    fix: removed spread operator

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 'use strict';
 
-const DEFAULT_OPTIONS = {
-	portNumber: 3001
-};
-
 class WildcardServerWebpackPlugin {
 	constructor(options) {
-		this.options = {
-			...DEFAULT_OPTIONS,
-			...options
-		};
+		this.portNumber = options.portNumber ? options.portNumber : 3000;
 	}
 
 	apply(compiler) {
-		const { portNumber } = this.options;
+		const portNumber = this.portNumber;
 		const { outputPath } = compiler;
 
 		const content = `var express = require('express');
@@ -22,14 +15,13 @@ var server = express();
 server.use('/${outputPath}', express.static(__dirname + '/${outputPath}'));
 
 server.get('/*', function(req, res) {
-    res.sendFile(__dirname + '/index.html);
+    res.sendFile(__dirname + '/index.html');
 });
 
 var port = ${portNumber};
 server.listen(port, function() {
     console.log('Server listening on port ' + ${portNumber});
-});
-        `;
+});`;
 
 		compiler.plugin('emit', function(compilation, callback) {
 			compilation.assets['server.js'] = {
